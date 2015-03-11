6d788d9c9dadff69e5fa4cc7860340a709cbc615
lawsn <lawsnland@gmail.com>
2015-03-11 21:16:22 +0900

    문서

---
+++ ipam/docs/ipam_init.sql
@@ -0,0 +1,1 @@
+INSERT INTO ipam_user (user_id, user_name, ip_list, other_desc, allow_excp, reg_date, change_date) VALUES('hong1', 'ȫ�浿1', '127.0.0.1,127.0.0.2', 'other_desc', 'f', '2015-03-04 12:29:07', '2015-03-04 12:29:07');
